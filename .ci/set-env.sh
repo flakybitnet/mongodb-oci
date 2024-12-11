ee98706b773367fe407efde44d5ff1df476df02a
0x1def <0x1def@flakybit.net>
2024-12-11 13:58:04 +0300

    Version bump

--- .ci/set-env.sh
+++ .ci/set-env.sh
@@ -14,7 +14,7 @@ app_component='server'
 printf 'APP_COMPONENT=%s\n' "$app_component" >> "$CI_ENV_FILE"
 
 printf 'APP_VERSION=%s\n' "$(getAppVersion)" >> "$CI_ENV_FILE"
-printf 'MONGO_VERSION=%s\n' '7.0.15' >> "$CI_ENV_FILE"
+printf 'MONGO_VERSION=%s\n' '7.0.16' >> "$CI_ENV_FILE"
 printf 'MONGO_SHELL_VERSION=%s\n' '2.3.3-0' >> "$CI_ENV_FILE"
 printf 'MONGO_TOOLS_VERSION=%s\n' '100.10.0' >> "$CI_ENV_FILE"
 printf 'MONGO_RUST_PING_VERSION=%s\n' '0.4.0' >> "$CI_ENV_FILE"
