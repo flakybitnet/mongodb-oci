9e339e8ddaa91950f587d9c9ee7d0884d45fa019
0x1def <0x1def@flakybit.net>
2025-07-07 12:48:37 +0300

    Version bump

--- .ci/set-env.sh
+++ .ci/set-env.sh
@@ -14,15 +14,15 @@ app_component='server'
 printf 'APP_COMPONENT=%s\n' "$app_component" >> "$CI_ENV_FILE"
 
 printf 'APP_VERSION=%s\n' "$(getAppVersion)" >> "$CI_ENV_FILE"
-mongo_version='8.0.10'
+mongo_version='8.0.11'
 printf 'MONGO_VERSION=%s\n' "$mongo_version" >> "$CI_ENV_FILE"
 printf 'MONGO_BINARIES_VERSION=%s\n' "$mongo_version-0" >> "$CI_ENV_FILE"
-printf 'MONGO_SHELL_VERSION=%s\n' '2.5.2-0' >> "$CI_ENV_FILE"
+printf 'MONGO_SHELL_VERSION=%s\n' '2.5.3-0' >> "$CI_ENV_FILE"
 printf 'MONGO_TOOLS_VERSION=%s\n' '100.12.2' >> "$CI_ENV_FILE"
 printf 'MONGO_RUST_PING_VERSION=%s\n' '0.4.0' >> "$CI_ENV_FILE"
-printf 'YQ_VERSION=%s\n' '4.45.1-8' >> "$CI_ENV_FILE"
-printf 'WAIT_PORT_VERSION=%s\n' '1.0.8-15' >> "$CI_ENV_FILE"
-printf 'RENDER_TEMPLATE_VERSION=%s\n' '1.0.7-15' >> "$CI_ENV_FILE"
+printf 'YQ_VERSION=%s\n' '4.45.4-3' >> "$CI_ENV_FILE"
+printf 'WAIT_PORT_VERSION=%s\n' '1.0.9-0' >> "$CI_ENV_FILE"
+printf 'RENDER_TEMPLATE_VERSION=%s\n' '1.0.8-0' >> "$CI_ENV_FILE"
 
 printf 'HARBOR_REGISTRY=%s\n' 'harbor.flakybit.net' >> "$CI_ENV_FILE"
 printf 'EXTERNAL_REGISTRY_NAMESPACE=%s\n' 'flakybitnet' >> "$CI_ENV_FILE"
